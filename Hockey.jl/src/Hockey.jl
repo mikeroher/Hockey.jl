93224c25f717c9f1ea8cf88f03b5f154579c630f
Michael Roher <roher4@gmail.com>
2019-02-27 21:25:40 -0500

    Move to package

---
+++ Hockey.jl/src/Hockey.jl
@@ -0,0 +1,5 @@
+module Hockey.jl
+
+greet() = print("Hello World!")
+
+end # module
--- test/runtests.jl
+++
@@ -1,7 +0,0 @@
-if VERSION < v"0.7.0-DEV.2005"
-    using Base.Test
-else
-    using Test
-end
-
-include("test_html_pbp.jl")
